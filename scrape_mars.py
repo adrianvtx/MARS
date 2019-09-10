4cc5b9987f125d10fd3efac62068d32314d1cfa2
adrianvtx <adrian.voj.2019@gmail.com>
2019-09-10 11:03:12 -0500

    cleanup and almost works

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # coding: utf-8
 
 # Mission_to_Mars 
@@ -9,9 +9,10 @@ import pandas as pd
 from splinter import Browser
 from splinter.exceptions import ElementDoesNotExist
 from bs4 import BeautifulSoup as bs
-import requests
 import time
-from pprint import pprint
+import requests
+
+output_dict = {}
 
 
 # URL of page to be scraped
@@ -34,33 +35,44 @@ from pprint import pprint
 
 
 
-executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=False)
 
+
+def init_browser(): 
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
 
 def scrape():
-# browser = init_browser()
+    browser = init_browser()
+    
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
 
-    time.sleep(1)
-
-
+    # time.sleep(1)
+    
     html = browser.html
     soup = bs(html, 'html.parser')
 
     nasa_t = soup.find("div",class_="content_title").text
     nasa_p = soup.find("div", class_="article_teaser_body").text #*****Paragraph Text??**********
 
+    browser.quit()
+    
     # print(nasa_t)
     # print(nasa_p)
+    
+    # Store data in a dictionary
+    output_dict = {
+        "nasa_t": nasa_t,
+        "nasa_p": nasa_p}
 
     # **********************************************
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    
+    browser = init_browser()
+
     browser.visit(url)
 
-    time.sleep(1)
-
+    time.sleep(3)
 
     html = browser.html
     soup = bs(html, 'html.parser')
@@ -73,6 +85,10 @@ def scrape():
     featured_image_url = main_url + img_url
 
     # print(featured_image_url)
+    browser.quit()
+
+    output_dict = {
+    "featured_image_url": featured_image_url}
 
     # ***************************************
     # def scrape():
@@ -80,9 +96,10 @@ def scrape():
     # url = 'https://mars.nasa.gov/news/'
     # browser.visit(url)
 
+    browser = init_browser()
 
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=False)
+    # executable_path = {'executable_path': 'chromedriver.exe'}
+    # browser = Browser('chrome', **executable_path, headless=False)
 
     twitter_url = 'https://twitter.com/marswxreport?lang=en'
     browser.visit(twitter_url)
@@ -93,8 +110,13 @@ def scrape():
     soup = bs(html, 'html.parser')
 
     mars_weather = soup.find('p', class_ = 'TweetTextSize TweetTextSize--normal js-tweet-text tweet-text').text
+    
     # print(mars_weather)
-
+    browser.quit()
+    output_dict = {
+        "mars_weather": mars_weather}
+    
+    # *************************************************************
 
     table_url = 'https://space-facts.com/mars/'
     mars_table = pd.read_html(table_url)
@@ -109,11 +131,23 @@ def scrape():
     # print(mars_df)
     # print('-------------------------------------------------------------------------------------------------------------------')
     # pprint(mars_html_table)
+    
+    output_dict = {
+        # "mars_df": mars_df,
+        "mars_html_table": mars_html_table}
+    
+    browser.quit()
 
 
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=False)
+# ******************************************************************************
+
+    browser = init_browser()
+
+    # executable_path = {'executable_path': 'chromedriver.exe'}
+    # browser = Browser('chrome', **executable_path, headless=False)
+    
     hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    
     browser.visit(hemi_url)
 
 
@@ -132,7 +166,9 @@ def scrape():
         img_url = stock_url + i_url
         hemisphere_image_urls.append({"title" : hemi, "img_url" : img_url})
         
-    hemisphere_image_urls
+    browser.quit()
+
+    # hemisphere_image_urls
 
     # hemisphere_image_urls = [
     #     {"title": "Valles Marineris Hemisphere", "img_url": "..."},
@@ -141,18 +177,22 @@ def scrape():
     #     {"title": "Syrtis Major Hemisphere", "img_url": "..."},
     # ]
 
-    mars_db = {  
-        "nasaTitle": nasa_t,
-        "nasaPgph": nasa_p,
-        "featureImage": featured_image_url,
-        "marsWeath": mars_weather,
-        "marsInfo": mars_df,
-        "marsTable": mars_html_table,
-        "imageList": hemisphere_image_urls
-        }
+    # mars_db = {  
+    #     "nasaTitle": nasa_t,
+    #     "nasaPgph": nasa_p,
+    #     "featureImage": featured_image_url,
+    #     "marsWeath": mars_weather,
+    #     "marsInfo": mars_df,
+    #     "marsTable": mars_html_table,
+    #     "imageList": hemisphere_image_urls
+    #     }
+    
+    
+    browser.quit()
 
-        # Close the browser after scraping
-# browser.quit()
+    output_dict = {
+    "hemisphere_image_urls": hemisphere_image_urls}
+
 
     # Return results
-    return mars_db
+    return output_dict
